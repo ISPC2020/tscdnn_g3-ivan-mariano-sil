cf2272f257117fb825b5a9b0be95f289305e1115
JULIO SACHI <jmarsachi@gmail.com>
2021-09-09 12:47:32 -0300

    ClasesparaCeldas1,2y3

---
+++ Celdas_1,2&3.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Celda 1
+# Import packages
+import os as os
+
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+sns.set_context("notebook")
+plt.style.use("fivethirtyeight")
+
+# Celda 2
+
+class Metodos initparam_actfunt:
+
+# Initialize parameters    
+    
+    def initialize_parameters(layers_dims):
+        """
+    Initialize parameters dictionary.
+    
+    Weight matrices will be initialized to random values from uniform normal
+    distribution.
+    bias vectors will be initialized to zeros.
+
+    Arguments
+    ---------
+    layers_dims : list or array-like
+        dimensions of each layer in the network.
+
+    Returns
+    -------
+    parameters : dict
+        weight matrix and the bias vector for each layer.
+    """
+    np.random.seed(1)               
+    parameters = {}
+    L = len(layers_dims)            
+
+    for l in range(1, L):           
+        parameters["W" + str(l)] = np.random.randn(
+            layers_dims[l], layers_dims[l - 1]) * 0.01
+        parameters["b" + str(l)] = np.zeros((layers_dims[l], 1))
+
+        assert parameters["W" + str(l)].shape == (
+            layers_dims[l], layers_dims[l - 1])
+        assert parameters["b" + str(l)].shape == (layers_dims[l], 1)
+
+    return parameters
+    
+    # Celda 3
+    # Define activation functions that will be used in forward propagation
+    def sigmoid(Z):
+        """
+    Computes the sigmoid of Z element-wise.
+
+    Arguments
+    ---------
+    Z : array
+        output of affine transformation.
+
+    Returns
+    -------
+    A : array
+        post activation output.
+    Z : array
+        output of affine transformation.
+    """
+    A = 1 / (1 + np.exp(-Z))
+
+    return A, Z
+
+    def tanh(Z):
+        """
+    Computes the Hyperbolic Tagent of Z elemnet-wise.
+
+    Arguments
+    ---------
+    Z : array
+        output of affine transformation.
+
+    Returns
+    -------
+    A : array
+        post activation output.
+    Z : array
+        output of affine transformation.
+    """
+    A = np.tanh(Z)
+
+    return A, Z
+
+    def relu(Z):
+        """
+    Computes the Rectified Linear Unit (ReLU) element-wise.
+
+    Arguments
+    ---------
+    Z : array
+        output of affine transformation.
+
+    Returns
+    -------
+    A : array
+        post activation output.
+    Z : array
+        output of affine transformation.
+    """
+    A = np.maximum(0, Z)
+
+    return A, Z
+
+    def leaky_relu(Z):
+        """
+    Computes Leaky Rectified Linear Unit element-wise.
+
+    Arguments
+    ---------
+    Z : array
+        output of affine transformation.
+
+    Returns
+    -------
+    A : array
+        post activation output.
+    Z : array
+        output of affine transformation.
+    """
+    A = np.maximum(0.1 * Z, Z)
+
+    return A, Z
+
